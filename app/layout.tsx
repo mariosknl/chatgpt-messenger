60d9f409bf6c84419f65e378565d9345bc484ae7
mariosknl <mariosknl@outlook.com>
2023-02-12 16:57:43 +0200

    login functionality implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,35 @@
-import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
+import Sidebar from "../components/Sidebar";
+import { SessionProvider } from "../components/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+import Login from "../components/Login";
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const session = await getServerSession(authOptions);
+
 	return (
 		<html>
 			<head />
 			<body>
-				<div className="flex">
-					<div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
-						<Sidebar />
-					</div>
-					{/* Client Provider - Notification */}
+				<SessionProvider session={session}>
+					{!session ? (
+						<Login />
+					) : (
+						<div className="flex">
+							<div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+								<Sidebar />
+							</div>
+							{/* Client Provider - Notification */}
 
-					<div className="bg-[#343541] flex-1">{children}</div>
-				</div>
+							<div className="bg-[#343541] flex-1">{children}</div>
+						</div>
+					)}
+				</SessionProvider>
 			</body>
 		</html>
 	);
